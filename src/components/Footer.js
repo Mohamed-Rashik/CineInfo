1a1e5f5c4782c9302183ecd4be8f4dee878ed607
Mohamed Rashik A <mohamedrashik@karomi.com>
2023-12-22 12:06:05 +0530

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,16 @@ export const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">CineInfo</Link>. All Rights Reserved.</span>
           <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
               <li>
-                  <a href="https://www.instagram.com/md_rsk_?igsh=NGVhN2U2NjQ0Yg==" target="_blank" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+                  <a href="https://www.instagram.com/md_rsk_?igsh=NGVhN2U2NjQ0Yg==" rel="noreferrer" target="_blank" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
               </li>
               <li>
-                  <a href="https://www.linkedin.com/in/mohamed-rashik-a-a64734221/" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+                  <a href="https://www.linkedin.com/in/mohamed-rashik-a-a64734221/" rel="noreferrer" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
               </li>
               <li>
-                  <a href="https://twitter.com/_md_rsk_" target="_blank" className="mr-4 hover:underline md:mr-6">Twitter</a>
+                  <a href="https://twitter.com/_md_rsk_" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Twitter</a>
               </li>
               <li>
-                  <a href="#" target="_blank" className="hover:underline">Email</a>
+                  <a href="https://github.com/Mohamed-Rashik" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Github</a>
               </li>
           </ul>
       </footer>
