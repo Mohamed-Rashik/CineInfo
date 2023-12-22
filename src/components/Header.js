83bbe76d88cb9302fcb3e7f42ef112e019030f52
Mohamed Rashik A <mohamedrashik@karomi.com>
2023-12-22 12:33:22 +0530

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,9 +31,9 @@ export const Header = () => {
   }
 
   useEffect(() => {
-    document.title = "Cinemate";
+    document.title = "CineInfo";
     return () => {
-      document.title = "Cinemate";
+      document.title = "CineInfo";
     };
   }, []);
   const updateTitle = (title) => {
@@ -46,8 +46,8 @@ export const Header = () => {
       <nav className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <Link to="/" className="flex items-center">
-            <img src={Logo} className="mr-2 h-6 sm:h-9" alt="Cinemate Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cinemate</span>
+            <img src={Logo} className="mr-2 h-6 sm:h-9" alt="CineInfo Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CineInfo</span>
           </Link>
 
           <div id="mobile-nav" className="flex md:order-2">
